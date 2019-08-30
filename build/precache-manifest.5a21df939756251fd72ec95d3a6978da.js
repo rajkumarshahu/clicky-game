433ad2ceda0e5bfa5fc875663fdfacaa42f3f523
Raj Kumar Shahu <rajkumarshahu@gmail.com>
2019-08-30 18:43:23 -0400

    updated package.json

---
+++ build/precache-manifest.5a21df939756251fd72ec95d3a6978da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5f807e005d52acc0a766cb3c2f08b83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "253b8814c0a782971756",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "cd65778a53a5ab20ce46",
+    "url": "/static/js/2.33649e61.chunk.js"
+  },
+  {
+    "revision": "253b8814c0a782971756",
+    "url": "/static/js/main.f18768bb.chunk.js"
+  },
+  {
+    "revision": "0ebfaf63642c8e34abd7",
+    "url": "/static/js/runtime~main.b027e18f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
